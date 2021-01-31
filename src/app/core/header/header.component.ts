3006ece33090297d32a0d469efb8306f9134e0f2
AkMANZHI <kumarabhishek83567@gmail.com>
2021-01-31 23:04:00 +0530

    Manage Login UI

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryISO, SearchCountryField, TooltipLabel } from 'ngx-intl-tel-input';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  separateDialCode = true;
+	SearchCountryField = SearchCountryField;
+	TooltipLabel = TooltipLabel;
+  CountryISO = CountryISO;
+  setCountry: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.setCountry = CountryISO.India;
   }
 
 }
--- src/app/module/app.module.ts
+++ src/app/module/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from '../core/core.module';
 import { IndexComponent } from './index/index.component';
-
+import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    NgxIntlTelInputModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
